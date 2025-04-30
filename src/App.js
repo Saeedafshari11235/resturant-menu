54ab7a30e05524ea13828d3c80efa5e19ef2b31b
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-04-30 21:35:18 +0330

    change base structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Menu</h2>
+          <div className="underline"></div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
