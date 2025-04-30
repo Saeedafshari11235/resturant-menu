962e44cc0e17d29b40f78003ad6a39b4423e4e54
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-04-30 21:49:01 +0330

    import categories

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import "./App.css";
+import menus from "./data";
+import Categories from "./Components/Categories";
+
+const allCategories = ["All", ...new Set(menus.map((item) => item.category))];
 
 function App() {
+  const [menu, setMenu] = useState(menus);
+  const [catogories, setCategories] = useState(allCategories);
+
   return (
     <main>
       <section className="menu section">
         <div className="title">
           <h2>Menu</h2>
-          <div className="underline"></div>
+          <div className="underline">
+            <Categories category={catogories}></Categories>
+          </div>
         </div>
       </section>
     </main>
